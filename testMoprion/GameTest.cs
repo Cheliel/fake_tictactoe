ac62b0bc27f42cec8d0269fd812c8e853fe71e27
etienne <etienne.pernon@gmail.com>
2024-12-19 12:14:29 +0100

    ADD => Xtest Project ADD => Player Test ADD => Game Test (useless)

---
+++ testMoprion/GameTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe;
+using TicTacToe.Display;
+using TicTacToe.Players;
+
+namespace testMoprion
+{
+    public class GameTest
+    {
+        [Fact]
+
+        public void GameSucess()
+        {
+            //arrange
+            IDisplay display = new ConsoleDisplay();
+            IPlayer player = new RandomPlayer('X');
+            IPlayer player2 = new RandomPlayer('O');
+            Game game = new Game(display, player, player2);
+            game.Play();
+        }
+}
---
+++ testMoprion/PlayerTest.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+using TicTacToe;
+using TicTacToe.Boards;
+using TicTacToe.Players;
+
+namespace testMoprion
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void Move()
+        {
+            //arrange
+            IPlayer player1 = new RandomPlayer('X');
+
+            //act
+            Result<PlayerMove> activeMove = player1.GetNextMove();
+
+            //assert
+            Assert.InRange(activeMove.Value.Row, 1, 3);
+            activeMove.Value.Column.Should().BeInRange(1, 3);
+
+        }
+
+    }
+}
